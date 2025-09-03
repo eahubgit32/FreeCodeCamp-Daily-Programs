44b9b0188d378414d280dcefebff8cb103e71ed4
eahubgit32 <eagithub32@gmail.com>
2025-09-03 19:33:31 +0800

    Delete Pangram directory

--- Pangram/Pangram.py
+++
@@ -1,7 +0,0 @@
-def is_pangram(sentence, letters):
-    set1 = set(ch.lower() for ch in sentence if ch.isalpha())
-    set2 = set(letters.lower())
-    
-    return set1 == set2 
-    
-is_pangram("hello", "helo")
